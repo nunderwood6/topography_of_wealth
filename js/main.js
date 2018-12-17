5302f2bbd43cfee650912f843fa8c9a9c8e6e554
Nick Underwood <nunderwood6@gmail.com>
2018-12-17 11:56:17 -0600

    Fixed scroll jank, added scenes

--- js/main.js
+++ js/main.js
@@ -10,18 +10,51 @@ function notify_complete()
     console.log('The image has been loaded into the browser cache.');
 }
 
-var images = [];
+//load growth images
+var growthImages = [];
 
 for(var i=1;i<10;i++){
-	var url = `data/img/000${i}.jpg`;
+	var url = `data/img/growth/000${i}.jpg`;
 	preloadImage(url);
-	images.push(url);
+	growthImages.push(url);
+}
+
+for(var i=10;i<16;i++){
+	var url = `data/img/growth/00${i}.jpg`;
+	preloadImage(url);
+	growthImages.push(url);
+}
+
+
+//load coast images
+var coastImages = [];
+
+for(var i=1;i<10;i++){
+	var url = `data/img/coast/000${i}.jpg`;
+	preloadImage(url);
+	coastImages.push(url);
 }
 
 for(var i=10;i<61;i++){
-	var url = `data/img/00${i}.jpg`;
+	var url = `data/img/coast/00${i}.jpg`;
 	preloadImage(url);
-	images.push(url);
+	coastImages.push(url);
+}
+
+//load disparity images
+
+var disparityImages = [];
+
+for(var i=1;i<10;i++){
+	var url = `data/img/disparity/000${i}.jpg`;
+	preloadImage(url);
+	disparityImages.push(url);
+}
+
+for(var i=10;i<61;i++){
+	var url = `data/img/disparity/00${i}.jpg`;
+	preloadImage(url);
+	disparityImages.push(url);
 }
 ////////////////////////////////////////////////////////////////////////
 //calculate padding
@@ -33,46 +66,123 @@ var pad= `${(bigW- imgW)/2}px`;
 d3.select("#myimg").style("padding-left", pad);
 
 /////////////////
-//create image tween
-var obj = {curImg: 0};
 
-var tween = TweenMax.to(obj, 5,{
-	curImg: images.length - 1,
+
+
+
+//create coast image tween
+var coastObj = {curImg: 0};
+
+var coast = TweenMax.to(coastObj, 5,{
+	curImg: coastImages.length - 1,
 	roundProps: "curImg",
 	immediateRender: true,
 	ease: Linear.easeNone,
 	onUpdate: function(){
-		$("#myimg").attr("src", images[obj.curImg]);
+		$("#myimg").attr("src", coastImages[coastObj.curImg]);
 	}
 });
 
+//create disparity image tween
+var disparityObj = {curImg: 0};
+
+var disparity = TweenMax.to(disparityObj, 5,{
+	curImg: disparityImages.length - 1,
+	roundProps: "curImg",
+	immediateRender: true,
+	ease: Linear.easeNone,
+	onUpdate: function(){
+		$("#myimg").attr("src", disparityImages[disparityObj.curImg]);
+	}
+});
+
+//create growth image tween
+var growthObj = {curImg: 0};
+
+var growth = TweenMax.to(growthObj, 5,{
+	curImg: growthImages.length - 1,
+	roundProps: "curImg",
+	immediateRender: true,
+	ease: Linear.easeNone,
+	onUpdate: function(){
+		$("#myimg").attr("src", growthImages[growthObj.curImg]);
+	}
+});
 
 //init controller
 var controller = new ScrollMagic.Controller();
 
 
-//create scene
-var firstFly = new ScrollMagic.Scene({
+//first pin img_sequence for duration
+var length = parseFloat(d3.select("#trigger").style("height"));
+
+var pin = new ScrollMagic.Scene({
 	triggerElement: "#trigger",
-	triggerHook: 0.1,
-	duration: '300vh',	//scene lasts for scroll distance of 100px
+	triggerHook: .9,
+	duration: length
 })
-	.setTween(tween)
-	.setPin("#img_sequence") //pins element for scene duration
-	.addIndicators()
+	.setPin("#img_sequence", {pushFollowers:false})
+	//.addIndicators()
 	.addTo(controller);
 
+//growth scene
+//set duration equal to height of step
+var d0 = parseFloat(d3.select("#growth").style("height"));
+
+var growthScene = new ScrollMagic.Scene({
+	triggerElement: "#growth",
+	triggerHook: .9,
+	duration: d0
+})	
+	.setTween(growth)
+	//.addIndicators()
+	.addTo(controller);
+
+//coast scene
+//set duration equal to height of text step
+var d1 = parseFloat(d3.select("#coast").style("height"));
+
+var coastScene = new ScrollMagic.Scene({
+	triggerElement: "#coast",
+	triggerHook: .9,
+	duration: d1	//scene lasts for scroll distance of ____
+})
+	.setTween(coast)
+	//.addIndicators()
+	.addTo(controller);
+
+//disparity scene
+//set duration equal to height of text step
+var d1 = parseFloat(d3.select("#disparity").style("height"));
+
+var disparityScene = new ScrollMagic.Scene({
+	triggerElement: "#disparity",
+	triggerHook: .9,
+	duration: d1	//scene lasts for scroll distance of ____
+})
+	.setTween(disparity)
+	//.addIndicators()
+	.addTo(controller);
+
+/*
+
+
+
+
+
+//set duration equal to height of text step
+var d1 = parseFloat(d3.select("#text1").style("height"));
 
 
 //first text scroll
 var firstText = new ScrollMagic.Scene({
 	triggerElement: "#text1",
 	triggerHook:1,
-	duration: 2000,
+	duration: d1,
 	reverse: true
 })
   .setPin("#img_sequence", {pushFollowers:false})
   .addIndicators()
   .addTo(controller);
-
+*/
 
