bcb3b4e679c9db6628fa4237935ad10ae3b29383
Nick Underwood <nunderwood6@gmail.com>
2019-01-31 23:25:53 -0600

    Updated Spanish translations

--- js/espanol.js
+++ js/espanol.js
@@ -1,26 +1,31 @@
 
 var languages = {
 	"English" : {
-	"t1": "Topography of Wealth in L.A.",
+	"t1": "Topography of Wealth",
+	"t1_1": "Wealth in L.A.",
 	"t2": "Visualizing Income Inequality as Terrain",
 	"t3": "Los Angeles has 58 billionaires and 58,000 homeless people. In a city where destitution coexists with opulence, vast gaps in opportunity exist between neighborhoods. Let's use physical terrain as a metaphor to visualize income inequality across LA.",
 	"t4": "Using 2017 census data, we displace each geographic unit vertically based on median annual household income. Low-income neighborhoods sink into canyons, while rich areas soar far above the city as high plateaus.",
-	"t6": "We set sea level equal to median US income, $59,000 per year, as a visual reference. Along the coast, affluent neighborhoods with incomes well above the US average form coastal mountain ranges.",
-	"t7": "The massive plateaus of the Beverly Hills area are likely even higher, as income estimates at this scale are capped and designated $250,000+.",
-	"t8": "Precipitous terrain reveals the stark inequity within the city. In some places, wealthy neighborhoods with incomes exceeding $250,000 lie immediately adjacent to others with income well below the poverty line.",
-	"t9": "The culture of the city reflects this unseen landscape. In particular, it can be heard in the sounds of L.A.'s rap music."
-
+	"t6": "Sea level is set equal to the median US income of $59,000 per year as a visual reference. Along the coast, affluent neighborhoods with incomes well above the US average form coastal mountain ranges which tower over less prosperous areas further inland.",
+	"t7": "This visualization technique almost certainly underestimates the gap between rich and poor. The massive plateaus of the Beverly Hills area are likely even higher, as census income estimates at this scale are capped and designated $250,000+.",
+	"t8": "Precipitous terrain reveals distopian levels of inequity within the city. In many places, wealthy neighborhoods with incomes exceeding $250,000 lie immediately adjacent to others with income well below the poverty line. Massive cliffs serve as an apt metaphor for severe local socioeconomic gaps given the long odds of upward social mobility.",
+	"t9": "This landscape is abstract but far from imaginary. Rather, it is achingly familiar to those living in its shadows, whose daily struggle is made torturous by their proximity to a lavish and unobtainable lifestyle. For profound echoes of their frustration and pain, look no further than the sounds of L.A.'s prolific hip-hop music scene.",
+	"t10": "From Compton, epicenter of West Coast rap, the view towards Beverly Hills helps us understand the futility expressed in lines such as these:<br><br><span class='lyrics'>'Would I survive to make it up out this hole in time?<br>...Compton made you believe success wasn't real<br>...I was rehearsing in repetition the phrase<br>Only one in a million will ever see better days<br>...I was terrified they'll be the last black boys to fly<br>Out of Compton'<br><br>-Kendrick Lamar, 'Black Boy Fly'</span><img src='data/img/kendrick_wikipedia.jpg' style='width:100%;height:auto;display:block;margin:0 auto;margin-top: 5px;border-radius:5px'><span class='att'>Photo: Wikimedia commons</span>Lamar's narrative masterpiece and second studio album 'Good kid, M.A.A.D. City' paints a vivid picture of his coming-of-age in a Compton troubled by economic and racial inequality. A homogenous definition of income inequality is insufficient: extreme and <a href='https://www.businessinsider.com/income-and-racial-inequality-maps-2015-5'>racialized inequity</a> continue to haunt not just LA, but our entire nation.",
+	"t11": "Extreme income inequality raises troubling questions about the health of our society and our capacity for empathy. Politicians are quick to trumpet figures such as gross GDP and unemployment rates as evidence for a thriving economy. Rarely do they paint the true picture, of a nation at once bloated and starving, marked by historic, persistent, and racialized economic inequality.",
+	"t12": `“...when you have seen hate-filled policemen curse, kick, brutalize, and even kill your black brothers and sisters with impunity; when you see the vast majority of your twenty million Negro brothers smothering in an airtight cage of poverty in the midst of an affluent society...then you will understand why we find it difficult to wait. There comes a time when the cup of endurance runs over and men are no longer willing to be plunged into an abyss of injustice where they experience the bleakness of corroding despair.” <br><br>-Martin Luther King, 1963 "Letter From Birmingham Jail" <br><br> <span style="font-style:normal;">How long will we wait?</span>`
 },
  "Español" : {
 	"t1": "Topografía de la Riqueza en L.A.",
 	"t2": "Visualizando la Desigualdad de Ingresos como Terreno",
 	"t3": "Los Angeles tiene 58 billionarios y 58,000 personas sin hogar. En una ciudad dónde la indigencia coexiste con la opulencia, enormes brechas de oportunidades existen entre los barrios. Usemos el terreno físico como una metáfora para visualizar la desigualdad de ingresos en LA.",
 	"t4": "Utilizando los datos del censo de 2017, desplazamos cada unidad geográfica verticalmente en función de los ingresos medios anuales de los hogares. Los barrios de bajos ingresos se hunden en cañones, mientras que las zonas ricas se elevan por encima de la ciudad como mesetas altas.",
-	"t6": "Establecemos el nivel del mar igual al ingreso medio de los Estados Unidos, $59,000 por año, como una referencia visual. Cerca de la costa, los barrios ricos con ingresos muy mayor del promedio de los Estados Unidos forman cordilleras costeras.",
-	"t7": "Las mesetas masivas del área de Beverly Hills probablemente están en realidad más altas, ya que las estimaciones de ingresos a esta escala están limitadas y se designan $250,000+.",
-	"t8": "Terreno precipitado revela la desigualdad severa dentro de la ciudad. En algunos lugares, los vecindarios ricos con ingresos que superan los $ 250,000 se encuentran inmediatamente adyacentes a otros con ingresos muy por debajo del umbral de pobreza.",
-	"t9": "La cultura de la ciudad refleja este paisaje invisible. En particular, se puede escuchar en los sonidos de la música rap de L.A."
-
+	"t6": "Se establece el nivel del mar igual al ingreso medio de los Estados Unidos, $59,000 por año, como una referencia visual. Cerca de la costa, los barrios ricos con ingresos muy mayor del promedio de los Estados Unidos forman cordilleras costeras que se elevan sobre áreas menos prósperas más hacia el interior..",
+	"t7": "Esta técnica de visualización probablemente subestima la brecha entre ricos y pobres. Las mesetas masivas del área de Beverly Hills probablemente están en realidad más altas, ya que las estimaciones de ingresos a esta escala están limitadas y se designan $250,000+.",
+	"t8": "El terreno precipitado revela niveles distópicos de inequidad dentro de la ciudad. En muchos lugares, los barrios ricos con ingresos que superan los $ 250,000 se encuentran inmediatamente adyacentes a otros con ingresos muy por debajo del umbral de pobreza. Los acantilados masivos sirven como una metáfora apta para las brechas socioeconómicas locales severas, dadas las largas probabilidades de movilidad social ascendente.",
+	"t9": "Este paisaje es abstracto pero lejos de lo imaginario. Al contrario, es dolorosamente familiar para aquellos que viven en sus sombras, cuya lucha diaria se ve torturada por su proximidad a un estilo de vida lujoso e inalcanzable. Para profundos ecos de su frustración y dolor, no busque más que los sonidos de la prolífica escena de hip hop musica de L.A.",
+	"t10": "Desde Compton, el epicentro del rap de la costa oeste, la vista hacia Beverly Hills nos ayuda a comprender la inutilidad expresada en líneas como éstas:<br><br><span class='lyrics'>'Would I survive to make it up out this hole in time?<br>...Compton made you believe success wasn't real<br>...I was rehearsing in repetition the phrase<br>Only one in a million will ever see better days<br>...I was terrified they'll be the last black boys to fly<br>Out of Compton'<br><br>-Kendrick Lamar, 'Black Boy Fly'</span><img src='data/img/kendrick_wikipedia.jpg' style='width:100%;height:auto;display:block;margin:0 auto;margin-top: 5px;border-radius:5px'><span class='att'>Photo: Wikimedia commons</span> La obra maestra narrativa de Lamar y su segundo álbum de estudio 'Good kid, M.A.A.D. City' muestra una imagen vívida de su mayoría de edad en un Compton con problemas de desigualdad económica y racial. Una definición homogénea de desigualdad de ingresos es insuficiente: extrema y <a href='https://www.businessinsider.com/income-and-racial-inequality-maps-2015-5'>desigualdad racializada</a> continúa persiguiendo no solo a LA, sino a toda nuestra nación.",
+	"t11": "La desigualdad extrema de ingresos pone preguntas preocupantes sobre la salud de nuestra sociedad y nuestra capacidad de empatía. Los políticos se apresuran a trompetear estadísticas como el PIB bruto y las tasas de desempleo como evidencia de una economía próspera. Rara vez pintan la pintura real de una nación hinchada y hambrienta a la vez, marcada por una desigualdad económica histórica, persistente y racializada.",
+	"t12": `“...when you have seen hate-filled policemen curse, kick, brutalize, and even kill your black brothers and sisters with impunity; when you see the vast majority of your twenty million Negro brothers smothering in an airtight cage of poverty in the midst of an affluent society...then you will understand why we find it difficult to wait. There comes a time when the cup of endurance runs over and men are no longer willing to be plunged into an abyss of injustice where they experience the bleakness of corroding despair.” <br><br>-Martin Luther King, 1963 "Letter From Birmingham Jail" <br><br> <span style="font-style:normal;">¿Cuánto tiempo esperaremos?</span>`
 }
 };
 
@@ -37,6 +42,11 @@ languageToggle.on("click", function(d){
 
 function switchLanguage(language) {
 		for(var textBlock in languages[language]){
-			d3.select("."+textBlock).text(languages[language][textBlock]);
+			if(textBlock=="t10" || textBlock=="t12"){
+				d3.select("."+textBlock).html(languages[language][textBlock]);
+			} else {
+			  d3.select("."+textBlock).text(languages[language][textBlock]);
+			}
+			
 		};
 }
